3aa840b13f76298451f0405f5a3256c9539adb9c
Patrick Maslen <patrick@quest-bird.com>
2017-05-06 16:27:57 +1000

    added class name to selection form

--- brp_flask.py
+++ brp_flask.py
@@ -10,10 +10,14 @@ app = Flask(__name__)
 @app.route("/")
 def choose_game():
     games_available = [cls() for cls in GameSystem.__subclasses__()]
+    game_name_class = {}
+    for game in games_available:
+        game_name_class[game.name] = type(game).__name__
     return render_template(
-    'choose_game.html',
-    title="Choose game system",
-    systems=games_available
+        'choose_game.html',
+        title="Choose game system",
+        systems=games_available,
+        game_class=game_name_class
     )
 
 if __name__ == "__main__":
