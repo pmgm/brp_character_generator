854960b01f7c662ab657b79f38235878e73221f1
Patrick Maslen <patrick@quest-bird.com>
2017-05-06 14:56:08 +1000

    added flask app (brp_flask) for web display of character generator

---
+++ brp_flask.py
@@ -0,0 +1,22 @@
+# model
+from dice_roller import *
+from game_system import *
+from genre import *
+from genre_brp_fantasy import *
+# visualisation
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def choose_game():
+    games_available = [cls() for cls in GameSystem.__subclasses__()]
+    #for idx, game_system in enumerate(games_available):
+    return render_template(
+    'choose_game.html',
+    title="Choose game system",
+    systems=games_available
+    )
+
+if __name__ == "__main__":
+    app.run()
+    
