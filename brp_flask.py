2e6e3987b280288df3f6f70b1e6c1b77c773b6bd
Patrick Maslen <patrick@quest-bird.com>
2017-06-17 17:38:46 +1000

    submit form from first (game) screen now goes to genre screen in Flask instatiates game object

--- brp_flask.py
+++ brp_flask.py
@@ -4,7 +4,7 @@ from game_system import *
 from genre import *
 from genre_brp_fantasy import *
 # visualisation
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -20,6 +20,18 @@ def choose_game():
         game_class=game_name_class
     )
 
+@app.route("/genre", methods = ['POST', 'GET'])
+def choose_genre():
+    if request.method == 'POST':
+        gamessystem = request.form['gamesystem']
+        # instatiate a game_system
+        bob = globals()[gamessystem]
+    return render_template(
+        'choose_genre.html',
+        title="Choose genre",
+        system= gamessystem
+    )
+    
 if __name__ == "__main__":
     app.run()
     
